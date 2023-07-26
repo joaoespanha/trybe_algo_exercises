2903b4fdd26bf32cd26364a79651ca2094c7aa88
Joao Espanha <jpbesr@gmail.com>
2023-07-26 16:35:53 -0300

    refactoring is_palindrome_iterative

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,11 +1,26 @@
 def is_palindrome_iterative(word):
     if not word:
         return False
-    low_index = 0
-    high_index = len(word) - 1
-    while low_index < high_index:
-        if word[low_index] != word[high_index]:
-            return False
-        low_index += 1
-        high_index -= 1
-    return True
+    start_index = 0
+    end_index = -1
+    is_palindrome = False
+    for _ in range(0, len(word)):
+        if word[start_index] == word[end_index]:
+            start_index += 1
+            end_index -= 1
+            is_palindrome = True
+        else:
+            is_palindrome = False
+            break
+    return is_palindrome
+
+
+if __name__ == "__main__":
+    palindrome1 = "asa"
+    palindrome2 = "SOCOS"
+    non_palindrome1 = "pedro"
+    non_palindrome2 = "agua"
+    print(is_palindrome_iterative(palindrome1))
+    print(is_palindrome_iterative(non_palindrome1))
+    print(is_palindrome_iterative(non_palindrome2))
+    print(is_palindrome_iterative(palindrome2))
