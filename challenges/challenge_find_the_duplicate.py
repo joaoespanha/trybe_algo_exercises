0a1b27866c69968cfd796575a105d5811637bb50
Joao Espanha <jpbesr@gmail.com>
2023-07-26 16:18:05 -0300

    refactoring find_duplicate func

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,6 +1,3 @@
-from collections import Counter
-
-
 def find_duplicate(nums):
     if (
         not nums
@@ -9,4 +6,14 @@ def find_duplicate(nums):
         or len(nums) == len(set(nums))
     ):
         return False
-    return Counter(nums).most_common(1)[0][0]
+    sorted_arry = sorted(nums)
+
+    for index in range(0, len(sorted_arry)):
+        if sorted_arry[index] == sorted_arry[index + 1]:
+            return sorted_arry[index]
+
+
+if __name__ == "__main__":
+    arry = [3, 1, 2, 4, 6, 5, 7, 7, 7, 8]
+
+    print(find_duplicate(arry))
